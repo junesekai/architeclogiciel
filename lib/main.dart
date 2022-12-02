62db7fe8aef65f7328fe833c3e7ca53b6294f86a
Amvame Adrien <amvamejonathan@gmail.com>
2022-12-02 07:36:10 +0100

    ajout du texte

--- lib/main.dart
+++ lib/main.dart
@@ -98,6 +98,7 @@ class _MyHomePageState extends State<MyHomePage> {
             const Text(
               'You have pushed the button this many times:',
             ),
+            const Text('CI CD GITHUB ACTIONS')
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headline4,
